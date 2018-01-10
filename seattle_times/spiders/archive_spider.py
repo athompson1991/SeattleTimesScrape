a216765dde9610b439ec4891c00ea5412c8ed128
athompson1991 <alexthompson1991@hotmail.com>
2018-01-09 21:20:43 -0800

    Basic scrape established

--- seattle_times/spiders/archive_spider.py
+++ seattle_times/spiders/archive_spider.py
@@ -3,18 +3,20 @@ import scrapy
 class ArchiveSpider(scrapy.Spider):
     name = "archives"
 
+    def __init__(self):
+        self.base_url = 'https://www.seattletimes.com/seattle-news/politics/page/'
+
     def start_requests(self):
-        urls = [
-            'https://www.seattletimes.com/seattle-news/politics/page/1/'
-            'https://www.seattletimes.com/seattle-news/politics/page/2/'
-            'https://www.seattletimes.com/seattle-news/politics/page/3/'
-        ]
+        urls = [self.base_url + str(i) + "/" for i in range(10)]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-1]
-        filename = 'archive-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        page_number = response.url.replace(self.base_url, "").replace("/", "")
+        article_listings = response.selector.css('.subsection').css('article.results-story')
+        ids = article_listings.xpath("@id").extract()
+        urls = article_listings.xpath("//article").css("h3.results-story-title").css("a::attr(href)").extract()
+        titles = article_listings.css("h3.results-story-title").css("a::text").extract()
+        publish_dates = article_listings.xpath("//article//div//time/@datetime").extract()
+        self.log('scraped page: %s' % page_number)
+        yield {'ids': ids, 'urls': urls, 'titles': titles, 'publish_dates': publish_dates}
